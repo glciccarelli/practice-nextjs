cc0d14d6810419420de4f51000412e6bf7fca4f8
Gianluca Ciccarelli <gianluca.ciccarelli25@gmail.com>
2022-04-11 20:31:20 -0300

    first commit

---
+++ pages/about.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/navbar'
+
+
+const About = () => {
+    return (
+        <div>
+            <Navbar />
+            <h1>About</h1>
+            <p>This is the about page</p>
+        </div>
+    )
+}
+
+export default About
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/navbar'
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar />
+            <h1>Hello world!</h1>
+        </div>
+    )
+}
+
+export default Home
